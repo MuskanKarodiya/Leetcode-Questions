df6b2beb5a34ce0ef1792d5e58bc184ef2061979
MuskanKarodiya <karodiyamuskan@gmail.com>
2024-11-27 20:11:54 +0530

    Time: 12 ms (61.24%), Space: 42.6 MB (7.24%) - LeetHub

---
+++ 1203-print-in-order/1203-print-in-order.java
@@ -0,0 +1,35 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+class Foo{
+    int turn;
+    public Foo() {
+        turn=0;
+    }
+
+    public synchronized void first(Runnable printFirst) throws InterruptedException {
+
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        turn=1;
+        notifyAll();
+    }
+
+    public synchronized void second(Runnable printSecond) throws InterruptedException {
+        while(turn!=1) {
+            wait();
+        }
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        turn=2;
+        notifyAll();
+    }
+
+    public synchronized void third(Runnable printThird) throws InterruptedException {
+        while(turn!=2){
+            wait();
+        }
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+
+    }
+}
