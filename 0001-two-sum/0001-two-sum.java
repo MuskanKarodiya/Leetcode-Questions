9e56e409031ca83e72d0cd8c1deda9fec575e35b
Muskan Karodiya <karodiyamuskan@gmail.com>
2025-01-24 10:39:35 +0530

    Time: 39 ms (40.44%), Space: 44.9 MB (60.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-         int n=nums.length;
+        int n=nums.length;
         for (int i = 0; i <n-1; i++) {
             for (int j = i+1; j <n ; j++) {
                 if(nums[i]+nums[j]==target) {
@@ -8,6 +8,6 @@ class Solution {
                 }
             }
         }
-        return new int[]{};
+        return null;
     }
 }
