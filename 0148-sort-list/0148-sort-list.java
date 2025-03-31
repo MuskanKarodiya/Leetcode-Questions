ab96880f938435e37b4506fedf0e085c6a79aa60
Muskan Karodiya <karodiyamuskan@gmail.com>
2025-03-31 19:53:49 +0530

    Time: 9 ms (94.81%), Space: 57.3 MB (17.12%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode midPrev = middleNode(head);
+        ListNode mid = midPrev.next;
+        midPrev.next = null; 
+        ListNode left=sortList(head);
+        ListNode right=sortList(mid);
+        return mergeTwoLists(left,right);
+    }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy=new ListNode();
+        ListNode curr=dummy;
+
+        while(list1!=null && list2!=null){
+            if(list2.val>list1.val){
+                curr.next=list1;
+                list1=list1.next;
+            }
+            else{
+                curr.next=list2;
+                list2=list2.next;
+            }
+            curr=curr.next;
+        
+        }  
+        curr.next=(list1!=null) ? list1:list2 ;
+        return dummy.next;
+    }
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;  // To track the node before the middle
+        
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return prev;  // Return the node before the middle
+    }
+}
