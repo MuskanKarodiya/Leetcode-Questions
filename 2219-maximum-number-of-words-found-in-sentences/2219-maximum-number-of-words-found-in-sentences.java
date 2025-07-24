cd836d158bf29a2f896f78ae2b1292c3926faeed
Muskan Karodiya <karodiyamuskan@gmail.com>
2025-07-24 21:33:52 +0530

    Time: 2 ms (97.57%), Space: 44.6 MB (17.6%) - LeetHub

--- 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.java
+++ 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.java
@@ -2,17 +2,11 @@ class Solution {
     public int mostWordsFound(String[] sentences) {
         
         int max=0;
-
-        for(String sentence:sentences){
-            sentence=sentence.trim();
-        
-
-            if(sentence.isEmpty()){
-               continue;
+        for(String ans : sentences){
+            int res=ans.split(" ").length;
+            if(res>max){
+                max=res;
             }
-            String[] words=sentence.split("\\s+");
-            max=Math.max(max,words.length);
-
         }
         return max;
     }
