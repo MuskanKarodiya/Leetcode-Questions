87aa33768fccccf736950f1ffe92bb2b28645c3b
Muskan Karodiya <karodiyamuskan@gmail.com>
2026-01-30 19:34:34 +0530

    Time: 1145 ms (13.17%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,5 +1,3 @@
 # Write your MySQL query statement below
 select e.name , b.bonus 
-from Employee e
-left JOIN Bonus b on e.empId=b.empId
-where b.bonus<1000 or b.bonus is null
+from Employee e left JOIN Bonus b on e.empId=b.empId where b.bonus<1000 or b.bonus is null;
